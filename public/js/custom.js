ab478e77978d42e4425098ea4c8f739b342a75e4
Katie Hoffman <ktahoffman@gmail.com>
2014-09-13 13:23:43 -0400

    rearrange to vertical positioning, text off center

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,19 +1,20 @@
 $(function() {
 
-  var width = 900,
-      height = 900;
+  var width = 1000,
+      height = 1000;
 
   var cluster = d3.layout.cluster()
-      .size([height, width - 160]);
+      .size([height, width - 200]);
 
-  var diagonal = d3.svg.diagonal()
-      .projection(function(d) { return [d.y, d.x]; });
+var diagonal = d3.svg.diagonal()
+  // Flip the values here.
+  .projection(function(d) { return [d.x, d.y]; });
 
   var svg = d3.select("body").append("svg")
       .attr("width", width)
       .attr("height", height)
     .append("g")
-      .attr("transform", "translate(40,0)");
+      .attr("transform", "translate(0,40)");
 
   d3.json("./public/js/test.json", function(error, root) {
     var nodes = cluster.nodes(root),
@@ -29,14 +30,14 @@ $(function() {
         .data(nodes)
       .enter().append("g")
         .attr("class", "node")
-        .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
+        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
 
     node.append("circle")
         .attr("r", 4.5);
 
     node.append("text")
-        .attr("dx", function(d) { return d.children ? -8 : 8; })
-        .attr("dy", 3)
+        .attr("dx", 2)
+        .attr("dy", function(d) { return d.children ? -8 : 8; })
         .style("text-anchor", function(d) { return d.children ? "end" : "start"; })
         .text(function(d) { return d.name; });
   });
