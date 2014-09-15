39cada4fe50b00be18222cb4168fe72683aadb0f
Katie Hoffman <ktahoffman@gmail.com>
2014-09-15 00:12:16 -0400

    fix json spelling, support cd .. and cd child

---
+++ public/js/animation.js
@@ -0,0 +1,44 @@
+var rootDirectory, active, folderName;
+
+function initDirectoryStructure(root) {
+  rootDirectory = root;
+
+  initNode(rootDirectory);
+  setActive(rootDirectory);
+}
+
+function initNode(node, parent) {
+  node.parent = parent;
+  node.g = $('#node-' + node.name);
+
+  if (node.children) {
+    node.children.forEach(function(child) {
+      initNode(child, node);
+    });
+  }
+}
+
+function setActive(newActive) {
+  if (!newActive) {
+    alert("no such directory!");
+    return;
+  }
+
+  if (active) {
+    active.g.removeClass("active");
+    active.g.find("circle").attr("r", "4.5");
+  }
+
+  active = newActive;
+  active.g.find("circle").attr("r", "12");
+  active.g.addClass("active");
+}
+
+function cd(name) {
+  if (name == "..") {
+    setActive(active.parent);
+  } else {
+    var child = active.children.filter(function(x) { return x.name == name; });
+    setActive(child[0]);
+  }
+}
--- public/js/input.js
+++ public/js/input.js
@@ -1,12 +1,13 @@
 $(function() {
 
-  // capture user input
   $("#submit").click(function(event) {
     event.preventDefault();
     event.stopPropagation();
-    var userInput = $("#command").val();
-
-    $("#result").text(userInput);
+    var command = $("#command");
+    var userInput = command.val();
+    command.val("");
+    var dir = userInput.match(/^cd (.*)/);
+    if (dir) cd(dir[1]);
   });
 
 });
