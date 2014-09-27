45537b100e5c9680c18aa8a9c171f267f588a7f1
Katie Hoffman <ktahoffman@gmail.com>
2014-09-27 11:23:26 -0400

    lines between circles broken

--- public/js/dendrogram.js
+++ public/js/dendrogram.js
@@ -16,31 +16,52 @@ $(function() {
       .attr("transform", "translate(0,40)");
 
   d3.json("./public/js/test.json", function(error, root) {
-    var nodes = cluster.nodes(root),
-        links = cluster.links(nodes);
 
-    var link = svg.selectAll(".link")
-        .data(links)
-      .enter().append("path")
-        .attr("class", "link")
-        .attr("d", diagonal);
+    initDirectoryStructure(root);
+    
+    var nodes = cluster.nodes(root),
+        linknks = cluster.links(nodes);
+
+    // var link = svg.selectAll(".link")
+    //     .data(links);
+    
+    // // link enter  
+    // var linkEnter = link.enter().append("path")
+    //     .attr("class", "link")
+    //     .attr("d", diagonal);
 
     var node = svg.selectAll(".node")
-        .data(nodes)
-      .enter().append("g")
+        .data(nodes);
+
+    // node enter
+    var nodeEnter = node.enter().append("g")
         .attr("id", function(d) { return "node-" + d.name; })
         .attr("class", "node")
-        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
 
-    node.append("circle")
+    nodeEnter.append("circle")
         .attr("r", 4.5);
 
-    node.append("text")
+    nodeEnter.append("text")
         .attr("dy", "-9")
         .style("text-anchor", "middle")
         .text(function(d) { return d.name; });
 
-    initDirectoryStructure(root);
+    // node update
+    var nodeUpdate = node.transition()
+        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+    // node exit
+    var nodeExit = node.exit().transition();
+
+    nodeExit.select("circle")
+      .attr("r", 1e-6);
+
+    nodeExit.select("text")
+      .style("fill-opacity", 1e-6);
+
+    nodeExit.remove();
+
   
   });
 
